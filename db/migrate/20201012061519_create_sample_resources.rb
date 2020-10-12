ac84bf57a53924fafb0c91de44463246b5198959
hakozaru <t.morii@everyleaf.com>
2020-10-12 15:16:48 +0900

    適当なスキーマを定義してmigrate

--- db/migrate/20201012061519_create_sample_resources.rb
+++ db/migrate/20201012061519_create_sample_resources.rb
@@ -1,7 +1,17 @@
 class CreateSampleResources < ActiveRecord::Migration
   def change
     create_table :sample_resources do |t|
-
+      t.string   "password", limit: 128
+      t.string   "hogehoge", limit: 128
+      t.string   "email"
+      t.string   "ip"
+      t.string   "last_login_ip"
+      t.integer  "login_count",        default: 0, null: false
+      t.datetime "login_at"
+      t.date "date_column"
+      t.integer  "integer_column"
+      t.boolean "boolean_column"
+      t.decimal "decimal_column", precision: 10, scale: 2
       t.timestamps null: false
     end
   end
